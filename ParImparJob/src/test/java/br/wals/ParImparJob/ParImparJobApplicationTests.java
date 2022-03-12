ba4d9df7975b2213ea423b1ce81b541eade0a5c8
wedersonals <wederson_adriano@hotmail.com>
2022-03-12 17:36:52 -0300

    Project working with chunk

---
+++ ParImparJob/src/test/java/br/wals/ParImparJob/ParImparJobApplicationTests.java
@@ -0,0 +1,13 @@
+package br.wals.ParImparJob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParImparJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
