4bb12b4bd4002001283115910d4a61a1223518e4
wedersonals <wederson_adriano@hotmail.com>
2022-04-01 07:50:28 -0300

    Projeto: implementando validações

---
+++ ProcessadorValidacaoJob/src/main/java/com/springbatch/processadorvalidacao/processor/ProcessadorValidacaoProcessorConfig.java
@@ -0,0 +1,46 @@
+package com.springbatch.processadorvalidacao.processor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.validator.ValidatingItemProcessor;
+import org.springframework.batch.item.validator.ValidationException;
+import org.springframework.batch.item.validator.Validator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.springbatch.processadorvalidacao.dominio.Cliente;
+
+@Configuration
+public class ProcessadorValidacaoProcessorConfig {
+
+	private Set<String> emails = new HashSet<>();
+	
+	@Bean
+	public ItemProcessor<Cliente, Cliente> procesadorValidacaoProcessor() {
+		//return cliente -> cliente;
+		
+//		BeanValidatingItemProcessor<Cliente> processor = new BeanValidatingItemProcessor<>();
+//		processor.setFilter(true);
+		
+		ValidatingItemProcessor<Cliente> processor = new ValidatingItemProcessor<>();
+		processor.setValidator(validator());
+		processor.setFilter(true);
+		
+		return processor;
+	}
+
+	private Validator<? super Cliente> validator() {
+		return new Validator<Cliente>() {
+
+			@Override
+			public void validate(Cliente cliente) throws ValidationException {
+				if (emails.contains(cliente.getEmail()))
+					throw new ValidationException(String.format("O cliente %s já foi processado!", cliente.getEmail()));
+				emails.add(cliente.getEmail());
+			}
+		};
+	}
+	
+}
