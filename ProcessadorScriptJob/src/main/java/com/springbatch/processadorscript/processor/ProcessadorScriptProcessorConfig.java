7046676a1b342b20dcd72182ef42e9962025cc4e
wedersonals <wederson_adriano@hotmail.com>
2022-04-07 13:54:24 -0300

    Projeto: utilizando JavaScript para processar arquivos

---
+++ ProcessadorScriptJob/src/main/java/com/springbatch/processadorscript/processor/ProcessadorScriptProcessorConfig.java
@@ -0,0 +1,24 @@
+package com.springbatch.processadorscript.processor;
+
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.support.builder.ScriptItemProcessorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.springbatch.processadorscript.dominio.Cliente;
+
+@Configuration
+public class ProcessadorScriptProcessorConfig {
+	
+	@Bean
+	public ItemProcessor<Cliente, Cliente> processadorScriptProcessor() {
+		return new ScriptItemProcessorBuilder<Cliente, Cliente>()
+				.language("nashorn")
+				.scriptSource(
+						"var email = item.getEmail();"
+						+ "var arquivoExiste = `ls /home/ | grep ${email}.txt`;" 
+						+ "if (!arquivoExiste) item; else null;"
+						)
+				.build();
+	}
+}
