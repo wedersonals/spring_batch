cef42d0d4087af84f5f95d99b89fd7868bcb02ba
wedersonals <wederson_adriano@hotmail.com>
2022-04-22 12:13:49 -0300

    Escritor de arquivo delimitado

--- ArquivoDelimitadoJob/src/main/java/br/wals/ArquivoDelimitadoJob/writer/LeituraArquivoDelimitadoWriterConfig.java
+++ ArquivoDelimitadoJob/src/main/java/br/wals/ArquivoDelimitadoJob/writer/LeituraArquivoDelimitadoWriterConfig.java
@@ -1,16 +1,28 @@
 package br.wals.ArquivoDelimitadoJob.writer;
 
-import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 import br.wals.ArquivoDelimitadoJob.dominio.Cliente;
 
 @Configuration
 public class LeituraArquivoDelimitadoWriterConfig {
 
+	@StepScope
 	@Bean
-	public ItemWriter<Cliente> leituraArquivoDelimitadoWriter() {
-		return items -> items.forEach(System.out::println);
+	public FlatFileItemWriter<Cliente> escritaArquivoDelimitadoWriter(
+			@Value("#{jobParameters['arquivoClientesSaida']}") Resource arquivoClientesSaida) {
+		return new FlatFileItemWriterBuilder<Cliente>()
+				.name("escritaArquivoDelimitadoWriter")
+				.resource(arquivoClientesSaida)
+				.delimited()
+				.delimiter("|")
+				.names("nome", "sobrenome", "idade", "email")
+				.build();
 	}
 }
