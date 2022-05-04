92ee7072aa9d52ca401caab445f113eac321d5b6
wedersonals <wederson_adriano@hotmail.com>
2022-05-03 23:49:14 -0300

    Projeto: migração de dados

---
+++ MigracaoDadosJob/src/main/java/br/wals/migracaodados/step/MigrarPessoaStepConfig.java
@@ -0,0 +1,34 @@
+package br.wals.migracaodados.step;
+
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.support.ClassifierCompositeItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import br.wals.migracaodados.dominio.Pessoa;
+
+@Configuration
+public class MigrarPessoaStepConfig {
+
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+	
+	@Bean
+	public Step migrarPessoaStep(
+			ItemReader<Pessoa> arquivoPessoaReader,
+			ClassifierCompositeItemWriter<Pessoa> pessoaClassifierWriter,
+			FlatFileItemWriter<Pessoa> arquivoPessoasInvalidasWriter) {
+		return stepBuilderFactory
+				.get("migrarPessoaStep")
+				.<Pessoa, Pessoa>chunk(1)
+				.reader(arquivoPessoaReader)
+				.writer(pessoaClassifierWriter)
+				.stream(arquivoPessoasInvalidasWriter)
+				.build();
+				
+	}
+}
